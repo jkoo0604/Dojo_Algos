e719916fff46e07320f5fcce53c5b1aa3d451ddf
jkoo0604 <63644766+jkoo0604@users.noreply.github.com>
2020-07-06 19:55:45 -0700

    0706

---
+++ Algo0706.js
@@ -0,0 +1,30 @@
+/**
+ * takes in an array
+ * returns THE SAME array sorted
+ * compares adjacent elements
+ * and swaps positions if necessary
+ * largest elements "bubble" to the top
+ * BONUS: fast exit if portion already sorted
+ */
+
+function bubbleSort(arr) {
+    // your code here
+  }
+  
+  console.log(bubbleSort([1, 5, 2, 8, 3, 4]));
+  // should log [1, 2, 3, 4, 5, 8]
+  
+  
+  /**
+   * takes in an array
+   * returns THE SAME array sorted
+   * each time through, select the smallest element
+   * at the end, swap it with the element at the current position
+   */
+  
+  function selectionSort(arr) {
+    // your code here
+  }
+  
+  console.log(selectionSort([1, 5, 2, 8, 3, 4]));
+  // should log [1, 2, 3, 4, 5, 8]
